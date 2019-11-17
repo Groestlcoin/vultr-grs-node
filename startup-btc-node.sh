fc185028e49e8d39770fc4945ddc24a6196f46f6
Jacc <6774432+jdabs@users.noreply.github.com>
2019-11-17 12:53:48 -0500

    Update startup-btc-node.sh

--- startup-btc-node.sh
+++ startup-btc-node.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
-# drop into a vultr.com VPS to install a Bitcoin full node
+# drop into a vultr.com Debian 9 VPS to install a Bitcoin full node 
+
+wget https://bitcoin.org/bin/bitcoin-core-0.18.1/bitcoin-0.18.1-x86_64-linux-gnu.tar.gz #09-Aug-2019 10:05
+tar xzf bitcoin-0.18.1-x86_64-linux-gnu.tar.gz
+install -m 0755 -o root -g root -t /usr/local/bin bitcoin-0.18.1/bin/*
+bitcoind -daemon
+# Bitcoin server starting = successful install
